0a300c8aa50a224ad94aa9f1c629e00921b2e5b4
lightserpent1 <chrismithel@gmail.com>
2024-09-02 16:19:57 -0700

    health counter works on enemy but bar doesnt visually changing

--- ranged_enemy.gd
+++ ranged_enemy.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+#health system
+const MAX_HEALTH = 5
+var health = MAX_HEALTH
+
 @onready var main = get_tree().get_root().get_node("testarea")
 @onready var projectile = load("res://projectile.tscn")
 var attack_started = false;
@@ -9,22 +13,22 @@ var attack_started = false;
 
 const SPEED = 300
 
-
-
-
-
 func _ready():
-	shoot()
-
-func shoot():
+	#shoot()
+	set_health_label()
+	$HealthBar.max_value = MAX_HEALTH
+	set_health_bar()
 	
+	
+	
+func shoot():
 	var instance = projectile.instantiate()
 	instance.dir = rotation
 	instance.spawnPos = global_position
 	instance.spawnRot = global_rotation
 	main.add_child.call_deferred(instance)
 
-func _process(delta):
+# func _process(delta):
 	if attack_started:
 		print("not attacking")
 		return
@@ -34,15 +38,27 @@ func _process(delta):
 		shoot()
 		attack_started = false
 
-
-
-func _physics_process(delta: float) -> void:
+#func _physics_process(delta: float) -> void:
 	look_at(target_to_chase.position)
 	if position.distance_to(target_to_chase.position) > 100:
 		navigation_agent.target_position = target_to_chase.global_position
 		velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * SPEED
 		move_and_slide()
 
+func set_health_label() -> void:
+	$HealthLabel.text = "Health: %s" % health
 
+func set_health_bar() -> void:
+	$HealthBar.value = health
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		damage()
+
+func damage() -> void:
+	health -= 1
+	if health < 0:
+		health = MAX_HEALTH
+	set_health_label()
+	set_health_bar()
 
