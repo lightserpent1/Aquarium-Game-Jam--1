56f6864a7b5f0fd47b735ab9c97472d974a5de9b
lightserpent1 <chrismithel@gmail.com>
2024-09-03 02:04:45 -0700

    working on ownerIs() to get bullets to not damage the enemy that shot them

--- ranged_enemy.gd
+++ ranged_enemy.gd
@@ -27,7 +27,8 @@ func shoot():
 	instance.spawnPos = global_position
 	instance.spawnRot = global_rotation
 	#main.add_child.call_deferred(instance)
-	self.add_child.call_deferred(instance)
+	instance.get_node("ProjectileArea2D").ownerIs(self)
+	main.add_child.call_deferred(instance)
 
 func _process(delta):
 	if attack_started:
