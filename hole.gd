d4ebd5e1b51a3745ba83a257107a17ad1c17652b
lightserpent1 <chrismithel@gmail.com>
2024-09-03 00:25:02 -0700

    Revert "Merge branch 'main' into enemy"
    This reverts commit 19f604d56141e71ea7f3c551534ae648aa0355de, reversing
    changes made to 11d262686a3bdcb23039af169615aa9b77d04b14.

--- hole.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var rng = RandomNumberGenerator.new()
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	await get_tree().create_timer(rng.randi_range(3-(Global.speedVariable/100+0.01),5-(Global.speedVariable/100+0.01))).timeout
-	queue_free()
--- random_wall_gen.gd
+++ random_wall_gen.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var wall_array = []
 var wall = preload("res://wall.tscn")
-var powerup
 var rng = RandomNumberGenerator.new()
 var neverEnd = true
 # Called when the node enters the scene tree for the first time.
@@ -11,23 +10,36 @@ func randX():
 	return(rng.randi_range(-700, 700))
 	
 func randY():
-	return(rng.randi_range(-350, 350))
+	return(rng.randi_range(-300, 300))
 	
+# Generates a random time between 1 and 3 seconds
+func randomBreakTimer():
+	return rng.randi_range(1, 3)
+
 # Called when the node is added to the scene
 func _ready() -> void:
 	# Spawn a random number of walls
 	spawnWalls()
 	# Start deleting walls after spawning
-	#deleteWalls()
+	deleteWalls()
 	while (neverEnd == true):
-		await get_tree().create_timer(rng.randi_range(1-(Global.speedVariable/100+0.01),3-(Global.speedVariable/100+0.01))).timeout
+		await get_tree().create_timer(10).timeout
 		spawnWalls()
-
+		deleteWalls()
 	
 # Spawns a wall at a given position and stores it in an array
 func spawnWalls():
-	for i in range(0, 1):
+	for i in range(0, rng.randi_range(5, 9)):
 		var instance = wall.instantiate()
 		instance.position = Vector2(randX(), randY())
 		wall_array.append(instance)
 		add_child(instance)
+
+# Deletes walls with a random delay between each deletion
+func deleteWalls():
+	for j in wall_array:
+		# Wait for a random amount of time before deleting the next wall
+		await get_tree().create_timer(1).timeout
+		j.queue_free()
+	# Clear the array after deleting all the walls
+	wall_array.clear()
