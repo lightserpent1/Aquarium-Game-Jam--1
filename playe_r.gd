66db02d1ccf741c661da0096b760c5373259cc9b
lightserpent1 <chrismithel@gmail.com>
2024-09-03 18:38:01 -0700

    gave player basic health bar

--- playe_r.gd
+++ playe_r.gd
@@ -5,6 +5,17 @@ class_name PlayerCharacter
 const max_speed = 400
 const acceleration = 1500
 const friction  = 1500
+#health system
+const MAX_HEALTH = 5
+var health = MAX_HEALTH
+
+
+func _ready():
+	#health system setup
+	set_health_label()
+	$HealthBar.max_value = MAX_HEALTH
+	set_health_bar()
+
 
 func player_movement(input, delta):
 
@@ -22,3 +33,25 @@ func _physics_process(delta):
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("power"):
 		print("test")
+
+
+func set_health_label() -> void:
+	$HealthLabel.text = "Health: %s" % health
+
+func set_health_bar() -> void:
+	$HealthBar.value = health
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		damage()
+
+func damage() -> void:
+	health -= 1
+	if health < 0:
+		queue_free()
+		health = MAX_HEALTH
+	set_health_label()
+	set_health_bar()
+
+func hit():
+	damage()
